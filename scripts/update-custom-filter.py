1042049034a2174a7b97c274d9dee833464e544f
Yazeed Omar <yazeed_o@yahoo.com>
2026-02-02 15:05:46 +0300

    Update font

--- scripts/update-custom-filter.py
+++ scripts/update-custom-filter.py
@@ -1,8 +1,8 @@
 import requests
 
-GF_Latin_All = "https://github.com/googlefonts/glyphsets/raw/main/GF_glyphsets/Latin/glyphs/CustomFilter_GF_Latin.plist"
-dest = "sources/CustomFilter_GF_Latin_All.plist"
+GF_Latin_All = "https://raw.githubusercontent.com/googlefonts/glyphsets/refs/heads/main/data/results/plist/CustomFilter_GF_Latin.plist"
+dest = "sources/CustomFilter_GF_Latin.plist"
 
-r = requests.get(GF_Latin_All)
+r = requests.get(GF_Latin)
 with open(dest, "wb") as f:
     f.write(r.content)
